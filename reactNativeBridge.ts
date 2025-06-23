b627a2c42d75e3bc2a4ed4e897e26048cd2ddf55
ywenhao <37856060+ywenhao@users.noreply.github.com>
2025-06-23 16:54:15 +0800

    Update reactNativeBridge.ts

--- reactNativeBridge.ts
+++ reactNativeBridge.ts
@@ -37,6 +37,8 @@ const setupOnmessage = () => {
       }
     }
   })
+
+  onmessageLoaded = true
 }
 
 const registerCallback = <T>(resolve: (value: T) => void, reject: (reason?: any) => void) => {
